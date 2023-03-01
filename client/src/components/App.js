9f292e705afb0e1339794f4c8eb6762a74a740e1
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-01 19:51:13 +0300

    updated

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Navbar from './Navbar';
 import Home from './Home';
 import Meme from './Meme';
 import Signup from './Signup';
+import Addpost from './Addpost';
+import Customisedmeme from './Customisedmeme';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
+  const [myMemes, setMyMemes] = useState([]);
   const navigate = useNavigate();
 
   const handleLogout = (e) => {
@@ -16,6 +18,19 @@ function App() {
     setIsAuthenticated(false);
     navigate('/');
   };
+
+  // fetch memes
+  useEffect(() => {
+    fetch("http://localhost:9292/memes")
+      .then((r) => r.json())
+      .then((memes) => setMyMemes(memes));
+  }, []);
+
+  function handleDeleteMessage(id) {
+    const updatedMemes = myMemes.filter((meme) => meme.id !== id);
+    setMyMemes(updatedMemes);
+  }
+
   return (
     <div className="App">
       <Navbar isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
@@ -23,10 +38,15 @@ function App() {
         <Route path="/" element={<Home setIsAuthenticated={setIsAuthenticated} />} />
         <Route path="/signup" element={<Signup setIsAuthenticated={setIsAuthenticated} />} />
         {isAuthenticated && (
-          <Route path="/shelf" element={<Meme />} />
+          <>
+            <Route path="/shelf" element={<Meme memes={myMemes} handleDeleteMessage={handleDeleteMessage} />} />
+            <Route path="/addpost" element={<Addpost setMyMemes={setMyMemes} />} />
+            <Route path="/customisedmeme" element={<Customisedmeme setMemes={setMyMemes} />} />
+          </>
         )}
       </Routes>
     </div>
   );
 }
+
 export default App;
