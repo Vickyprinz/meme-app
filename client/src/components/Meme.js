7472009ec1be140ee5a2ad4cf26174bf28190a06
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-01 18:46:12 +0300

    updated

--- client/src/components/Meme.js
+++ client/src/components/Meme.js
@@ -2,7 +2,7 @@ import Add_post from "./Add_post";
 import {useState, useEffect} from "react";
 
 const Meme = () => {
-  const [myMemes, setMyMemes] = useState([])
+  const [Memes, setMyMemes] = useState([])
 
 }
 export default Meme
