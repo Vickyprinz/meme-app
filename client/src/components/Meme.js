0bf387384261176facaa39957f4975b017b9e23f
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-01 18:50:22 +0300

    updated to fetch data from database

--- client/src/components/Meme.js
+++ client/src/components/Meme.js
@@ -2,7 +2,24 @@ import Add_post from "./Add_post";
 import {useState, useEffect} from "react";
 
 const Meme = () => {
-  const [Memes, setMyMemes] = useState([])
+  const [Memes, setMemes] = useState([])
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:9292/users/1');
+        const userData = await response.json();
+        console.log(userData);
+        setUserData(userData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    fetchUserData();
+  }, []);
+  
+
+  
 }
 export default Meme
