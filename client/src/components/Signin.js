e40d263de9e1580f52d190fd975c6816f491fe02
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-01 13:49:06 +0300

    Add form input fields to Signin component

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -23,4 +23,40 @@ class Signup extends Component {
         }
       });
   };
+
+  render() {
+    const { username, password } = this.state;
+    return (
+      <div>
+        <form className="text-center border border-light p-5">
+          <p className="h4 mb-4">Sign up</p>
+          <input
+            name='username'
+            id="defaultSignup"
+            onChange={this.handleChange}
+            value={username}
+            className="form-control mb-4"
+            placeholder="Username"
+          />
+          <input
+            name='password'
+            type='password'
+            id="defaultSignupPassword"
+            onChange={this.handleChange}
+            value={password}
+            className="form-control mb-4"
+            placeholder="Password"
+          />
+          <button
+            onClick={this.handleSubmit}
+            className="btn btn-info btn-block my-4"
+            type="submit">
+            Sign up
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default Signup;
