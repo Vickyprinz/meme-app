0c703795fb29cca121d2e19c178ca3cc0e66ac59
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-01 18:53:15 +0300

    added code that maps over the "myMemes" array using the "map" method and returns a new array of JSX elements

--- client/src/components/Meme.js
+++ client/src/components/Meme.js
@@ -19,7 +19,13 @@ const Meme = () => {
     fetchUserData();
   }, []);
   
-
+  const allMemes = myMemes.map(meme => (
+    <div key={meme.id} className="text-white p-4 mt-5 bg-slate-400 border ml-20 rounded-xl shadow">
+      <p className=""><span className="text-black-900 font semi-bold">title: </span>{meme.title}</p>
+      <p className=""><span className="text-black-900 font semi-bold">punchline: </span>{meme.message}</p>
+    </div>
+  ));
   
+
 }
 export default Meme
