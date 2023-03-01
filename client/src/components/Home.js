670a9c93aebe5f21bda48417178ccb7326d3feb5
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-01 14:50:14 +0300

    imports

---
+++ client/src/components/Home.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
+
+import Navbar from './Navbar';
+import Home from './Home';
+import Meme from './Meme';
+import Register from './Register'
