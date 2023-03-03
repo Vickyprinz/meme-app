da3c37aebac49a83374caa0a1c0e92f701ffe847
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-03 12:05:04 +0300

    added users to be logged in to use the application:

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < Sinatra::Base
     enable :sessions
   end
 
+  before do
+    authenticate
+  end
+
   # fetch all memes
   get '/memes' do
     memes = Meme.order(:created_at)
@@ -12,37 +16,37 @@ class ApplicationController < Sinatra::Base
 
   # delete meme
   delete '/memes/:id' do
-    meme = Meme.find_by(id: params[:id], user_id: session[:user_id])
-    if meme
-      meme.destroy
-      { message: 'Meme deleted successfully' }.to_json
+    memes = Meme.find(params[:id])
+    if memes.user == current_user
+      memes.destroy
+      memes.to_json
     else
-      { error: 'Meme not found or you are not authorized to delete this meme' }.to_json
+      { error: 'You are not authorized to delete this meme' }.to_json
     end
   end
 
   # update a meme
   patch '/memes/:id' do
-    meme = Meme.find_by(id: params[:id], user_id: session[:user_id])
-    if meme
-      meme.update(
+    memes = Meme.find(params[:id])
+    if memes.user == current_user
+      memes.update(
         title: params[:title],
         message: params[:message]
       )
-      meme.to_json
+      memes.to_json
     else
-      { error: 'Meme not found or you are not authorized to update this meme' }.to_json
+      { error: 'You are not authorized to update this meme' }.to_json
     end
   end
 
   # create a new meme
   post '/memes' do
-    meme = Meme.create(
+    memes = Meme.create(
       title: params[:title],
       message: params[:message],
-      user_id: session[:user_id]
+      user_id: current_user.id
     )
-    meme.to_json
+    memes.to_json
   end
 
   # fetch a user with their memes
@@ -78,9 +82,8 @@ class ApplicationController < Sinatra::Base
 
   # get current user
   get '/me' do
-    if session[:user_id]
-      user = User.find(session[:user_id])
-      { username: user.username, email: user.email }.to_json
+    if current_user
+      { username: current_user.username, email: current_user.email }.to_json
     else
       { error: 'Not authenticated' }.to_json
     end
@@ -97,5 +100,15 @@ class ApplicationController < Sinatra::Base
     query = params[:q]
     memes = Meme.where("title LIKE ? OR created_at LIKE ?", "%#{query}%", "%#{query}%").order(:created_at)
     memes.to_json
+  end
+
+  private
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def authenticate
+    halt 401, { error: 'Not authenticated' }.to_json unless current_user
   end
 end
